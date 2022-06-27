c9be9159f9f00087e171537aa4e19ed5a299e678
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-28 01:37:51 +0200

    shows customers

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 import express from 'express';
+import mysql from 'mysql';
 
 const app = express();
 const port = 3032;
 
 app.get('/', (req, res) => {
     res.send('test api');
+});
+
+app.get('/customers', (req, res) => {
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'webuser',
+        password: 'passpass',
+        database: 'northwind'
+    });
+    connection.connect((err) => {
+        if (err) throw err;
+        const sql = 'SELECT company,last_name,first_name FROM customers';
+        connection.query(sql, (err, records) => {
+            if (err) throw err;
+            res.send(records);
+        });
+    });
 });
 
 app.listen(port, () => {
